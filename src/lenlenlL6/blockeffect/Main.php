0af7f798b6a3fc06ff2b85f10d852b192327f77b
Karyl <62021442+lenlenlL6@users.noreply.github.com>
2022-07-06 20:25:40 +0700

    Create Main.php

---
+++ src/lenlenlL6/blockeffect/Main.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace lenlenlL6\blockeffect;
+
+use pocketmine\utils\Config;
+use pocketmine\plugin\PluginBase;
+use lenlenlL6\blockeffect\EventListener;
+
+final class Main extends PluginBase{
+  
+  public Config $config;
+  
+  public function onEnable() : void{
+  $this->saveDefaultConfig();
+  $this->config = $this->getConfig();
+  $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+  }
+}
